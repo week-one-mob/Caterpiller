c1231d23e39686a6cf62e5002552c1cc8c996faa
Dirt Deodara <thecrowleaps@gmail.com>
2019-04-25 16:35:52 -0700

    cleaned up our code a bit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-//import need functions
 import feedCaterpillar from './feedCaterpillar.js';
 
-//get DOM elements
+
 const fruitButtons = document.querySelectorAll('.fruit');
 const caterpillar = document.getElementById('caterpillar');
 
-//initialize things
 for(let i = 0; i < fruitButtons.length; i++){
     const fruitButton = fruitButtons[i];
 
