4382dec5bd8f6aa878661101cf745d13d4887f9c
Dirt Deodara <thecrowleaps@gmail.com>
2019-04-25 14:57:19 -0700

    added DOM elements started for loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+//import need functions
+
 //get DOM elements
+const fruitButtons = document.querySelectorAll('.fruit');
+const caterpiller = document.getElementById('caterpiller');
+//initialize things
+for(let i = 0; i < fruitButtons.length; i++){
+    const fruitButton = fruitButtons[i];
+
+    fruitButtons.addEventListener('click', () => {
+        feedCaterpillar()
+    });
+}
+//dancing
+
+//remove previous class from all
