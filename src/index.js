f025c0dc49dcba3ad57d4fc009bbf1e0bdf35c68
Dirt Deodara <thecrowleaps@gmail.com>
2019-04-25 16:28:36 -0700

    forced our caterpillar to eat

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 //import need functions
+import feedCaterpillar from './feedCaterpillar.js';
 
 //get DOM elements
 const fruitButtons = document.querySelectorAll('.fruit');
 const caterpillar = document.getElementById('caterpillar');
+
 //initialize things
 for(let i = 0; i < fruitButtons.length; i++){
-    const fruitButtons = fruitButtons[i];
+    const fruitButton = fruitButtons[i];
 
-    fruitButtons.addEventListener('click', () => {
-        feedCaterpillar()
+    fruitButton.addEventListener('click', () => {
+        feedCaterpillar(caterpillar, fruitButton.value);
     });
 }
+
 //dancing
 
 //remove previous class from all
